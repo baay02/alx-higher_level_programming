8cd908d48375aeae4e2323dfec30dc9f4055c401
baay02 <baaybilya25@yahoo.com>
2022-12-02 21:05:11 -0800

    all t@skcommit

---
+++ 0x03-python-data_structures/lists.h
@@ -0,0 +1,24 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+int is_palindrome(listint_t **head);
+void print_python_list_info(PyObject *p);
+
+#endif /* LISTS_H */
+
