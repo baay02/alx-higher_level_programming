590613bdbcefee29b2dcb1febbcf8c3263b9d5fe
baay02 <baaybilya25@yahoo.co>
2023-03-31 05:38:57 -0700

    python script that fetch url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-"""A script that
-fetches https://intranet.hbtn.io/status.
-"""
+""" a python script to open a url """
 
+from urllib.request import urlopen
 
-if __name__ == '__main__':
-    import urllib.request
-
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
-        content = resp.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+if __name__ == "__main__":
+    with urlopen("https://alx-intranet.hbtn.io/status") as f:
+        body = f.read()
+        print('Body response:')
+        print(f'\t- type: {type(body)}')
+        print(f'\t- content: {body}')
+        print(f'\t - utf8 content: {body.decode("utf-8")}')
